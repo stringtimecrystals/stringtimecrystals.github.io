cd63df35452a4e12513f7849d6ce04435efb12a7
esculapeso <esculapeso@gmail.com>
2024-09-15 13:40:26 +0200

    initial commit

---
+++ slides.js
@@ -0,0 +1,36 @@
+const slidesData = [
+    {
+      id: 1,
+      languages: {
+        en: {
+          image: "images/time_crystal_holo.gif",
+          title: "Slide 1 - English",
+          content: "<p>This is the content for the first slide in English.</p>",
+          spoken: "<p>This is the spoken version of slide 1 in English.</p>"
+        },
+        pl: {
+          title: "Slajd 1 - Polski",
+          content: "<p>To jest treść pierwszego slajdu po polsku.</p>",
+          spoken: "<p>To jest wersja mówiona slajdu 1 po polsku.</p>"
+        }
+      }
+    },
+    {
+      id: 2,
+      languages: {
+        en: {
+          image: "https://via.placeholder.com/200",
+          title: "Slide 2 - English",
+          content: "<p>This is the content for the second slide in English.</p>",
+          spoken: "<p>This is the spoken version of slide 2 in English.</p>"
+        },
+        pl: {
+          image: "https://via.placeholder.com/200",
+          title: "Slajd 2 - Polski",
+          content: "<p>To jest treść drugiego slajdu po polsku.</p>",
+          spoken: "<p>To jest wersja mówiona slajdu 2 po polsku.</p>"
+        }
+      }
+    }
+  ];
+  
